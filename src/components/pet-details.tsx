b8ea0eea4b8b4b073fb081bbde76a481589e96b2
Justin OConnell <itsjustinoconnell@gmail.com>
2024-04-04 13:40:21 +0100

    pet details styled

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -2,11 +2,49 @@
 
 import { usePetContext } from "@/contexts/PetContextProvider";
 
+import Image from "next/image";
+
 export default function PetDetails() {
-  const { currentPetId, pets } = usePetContext();
-  const currentPet = pets.filter((p) => p.id === currentPetId);
+  const { petDetails } = usePetContext();
+  if (!petDetails) return;
 
-  console.log(currentPet);
+  return (
+    <section className="h-full w-full overflow-scroll md:overflow-auto">
+      <div className="flex items-center border-b border-black/10 bg-white px-5 py-5">
+        <Image
+          src={petDetails.imageUrl}
+          alt={petDetails.name}
+          height={75}
+          width={75}
+          className="h-[75px] w-[75px] rounded-full object-cover"
+        />
+        <h2 className="ml-5 text-3xl font-semibold leading-6">
+          {petDetails.name}
+        </h2>
+      </div>
 
-  return <section className="h-full w-full">Pet Details</section>;
+      <div className="mt-10 flex items-center justify-around">
+        <div>
+          <h3 className="text-center text-[15px] font-medium uppercase text-zinc-700">
+            owner name
+          </h3>
+          <p className="mt-2 text-center text-lg text-zinc-800">
+            {petDetails.ownerName}
+          </p>
+        </div>
+        <div>
+          <h3 className="text-center text-[15px] font-medium uppercase text-zinc-700">
+            age
+          </h3>
+          <p className="mt-2 text-center text-lg text-zinc-800">
+            {petDetails.age}
+          </p>
+        </div>
+      </div>
+
+      <div className="mx-auto mt-5 h-[200px] w-10/12 rounded-md border border-black/10 bg-white px-7 py-5 shadow-sm md:h-[350px]">
+        {petDetails.notes}
+      </div>
+    </section>
+  );
 }
--- src/contexts/PetContextProvider.tsx
+++ src/contexts/PetContextProvider.tsx
@@ -6,6 +6,7 @@ type PetContextProps = {
   pets: Array<PetData>;
   handleChangePetId: (id: string) => void;
   currentPetId: string | null;
+  petDetails: PetData | undefined;
 };
 
 type PetContextProviderProps = {
@@ -18,12 +19,14 @@ const PetContext = createContext<PetContextProps | null>(null);
 function PetContextProvider({ data, children }: PetContextProviderProps) {
   const [pets, setPets] = useState(data);
   const [currentPetId, setCurrentPetId] = useState<null | string>(null);
+  const petDetails = pets.find((pet) => pet.id === currentPetId);
   const handleChangePetId = (id: string) => setCurrentPetId(id);
   return (
     <PetContext.Provider
       value={{
         pets,
         currentPetId,
+        petDetails,
         handleChangePetId,
       }}
     >
