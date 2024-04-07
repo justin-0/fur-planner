f5dc252c4219e993423682267df066d595c31526
Justin OConnell <itsjustinoconnell@gmail.com>
2024-04-07 13:28:08 +0100

    form values populated with current pet when edit form is opened

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -13,7 +13,7 @@ type PetFormProps = {
 };
 
 export default function PetForm({ action, onSubmission }: PetFormProps) {
-  const { handleClickAddPet, numberOfPets } = usePetContext();
+  const { handleClickAddPet, numberOfPets, petDetails } = usePetContext();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -36,23 +36,50 @@ export default function PetForm({ action, onSubmission }: PetFormProps) {
     <form className="space-y-4 text-left" onSubmit={handleSubmit}>
       <div className="space-y-2 ">
         <Label htmlFor="name">Name</Label>
-        <Input id="name" name="name" type="text" required />
+        <Input
+          id="name"
+          name="name"
+          type="text"
+          required
+          defaultValue={action === "edit" ? petDetails?.name : ""}
+        />
       </div>
       <div>
         <Label htmlFor="ownerName">Owners Name</Label>
-        <Input id="ownerName" name="ownerName" type="text" required />
+        <Input
+          id="ownerName"
+          name="ownerName"
+          type="text"
+          required
+          defaultValue={action === "edit" ? petDetails?.ownerName : ""}
+        />
       </div>
       <div>
         <Label htmlFor="ownerName">Image Url</Label>
-        <Input id="imageUrl" name="imageUrl" type="text" />
+        <Input
+          id="imageUrl"
+          name="imageUrl"
+          type="text"
+          defaultValue={action === "edit" ? petDetails?.imageUrl : ""}
+        />
       </div>
       <div>
         <Label htmlFor="age">Age</Label>
-        <Input id="age" name="age" type="number" required />
+        <Input
+          id="age"
+          name="age"
+          type="number"
+          required
+          defaultValue={action === "edit" ? petDetails?.age : ""}
+        />
       </div>
       <div>
         <Label htmlFor="notes">Notes</Label>
-        <Textarea id="notes" name="notes" />
+        <Textarea
+          id="notes"
+          name="notes"
+          defaultValue={action === "edit" ? petDetails?.notes : ""}
+        />
       </div>
       <Button type="submit" className="mr-auto">
         {action === "add" ? `Add new pet` : `Update pet`}
